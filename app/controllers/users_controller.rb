9ec789665a6633f9f60eee20c939fcb25358084a
TarrinRos <tarindis@gmail.com>
2018-12-01 00:48:36 +0200

    Add partials for user and question views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,14 @@ class UsersController < ApplicationController
 
   def show
     @user = User.new(
-                  name: 'Alex',
-                  username: 'alexatar',
-    avatar_url: 'https://ru.gravatar.com/userimage/21555925/f87b9376098bcb73b7c4a7f1819c802e.png'
+      name: 'Alex',
+      username: 'alexatar',
+      avatar_url: 'https://ru.gravatar.com/userimage/21555925/f87b9376098bcb73b7c4a7f1819c802e.png'
     )
+
+    @questions = [
+      Question.new(text: 'Hello, how do you do?', created_at: Date.parse('01.12.2018')),
+      Question.new(text: 'What?', created_at: Date.parse('02.12.2018'))
+    ]
   end
 end
