c6b8e495a5c65cba9ffc5edd5176a332f5e19671
TarrinRos <tarindis@gmail.com>
2018-12-09 18:47:25 +0200

    Add assignment and showing for questions author

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -29,4 +29,8 @@ module QuestionsHelper
       vopros
     end
   end
+
+  def get_author_username(question)
+    User.find(question.author_id).username
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,2 @@
 module UsersHelper
-  def username_by_user_id_in_questions(question)
-    User.find(question.user_id).username
-  end
 end
