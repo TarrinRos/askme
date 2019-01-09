44b1d9adeaff741a5822daf8cb42ac94bf89cf0f
TarrinRos <tarindis@gmail.com>
2019-01-09 17:29:51 +0200

    Delete aliases from routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,4 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :questions, except: [:show, :new, :index]
   resources :hashtags, only: [:show], param: :name
-
-  get 'show' => 'users#show'
-
-  get 'sign_up' => 'users#new'
-  get 'log_in' => 'sessions#new'
-  get 'log_out' => 'sessions#destroy'
 end
