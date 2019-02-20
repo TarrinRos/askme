2db5c2a4a61189c52a75872ff8e6c127e8cf04b2
TarrinRos <tarindis@gmail.com>
2019-02-20 17:15:27 +0200

    ActiveAdmin initial install

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root "users#index"
   resources :users
   resource :session, only: [:new, :create, :destroy]
