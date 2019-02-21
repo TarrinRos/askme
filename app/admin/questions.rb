8649b22dd117d079b4864208e6728d4cf9cce37f
TarrinRos <tarindis@gmail.com>
2019-02-21 12:14:55 +0200

    Add scopes for Question in ActiveAdmin

--- app/admin/questions.rb
+++ app/admin/questions.rb
@@ -1,15 +1,13 @@
 ActiveAdmin.register Question do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  scope("answered") { |scope| scope.where.not(answer: nil) }
+  scope("unanswered") { |scope| scope.where(answer: nil) }
 
+  index do
+    column 'To user', :user
+    column :text
+    column :answer
+    column :author
+    column 'Published at',:created_at
+    actions
+  end
 end
